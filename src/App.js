c17700bf532c5814fca460c4045ccc7993367835
Ogunriwa <134988853+Ogunriwa@users.noreply.github.com>
2023-11-29 21:01:26 -0600

    commift after

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 
 import Main from '../Main/Main';
-import Navbar from '../Navbar/Navbar';
-import footer from '../footer/footer';
+import Navbar from '../Navbar/Navbar'
+import Footer from '../Footer/Footer';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className= {styles.Backcolor}>
       <Navbar/>
       <Main/>
+      <Footer/>
     
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+
+    return (
+    <div className= {styles.navitem}>
+        <li>About Us</li>
+        <li>Let's Talk</li>
+
+    </div>
+    )
+}
