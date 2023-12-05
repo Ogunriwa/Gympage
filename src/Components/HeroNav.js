fed4fb0741ca485124ce63e8b83e8b9df696fb5d
Ogunriwa <134988853+Ogunriwa@users.noreply.github.com>
2023-12-04 21:16:29 -0600

    navbar 12done

---
+++ src/Components/HeroNav.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Hero from "./Hero"
+import Navbar from "./Navbar";
+
+
+const HeroNav = () => {
+    
+}
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,25 @@
 import React from "react";
 import styles from './Navbar.module.css';
 
+const images = {
+    image1 : "/Users/ibrahimarogundade/Desktop/gym_pages/Gympage/src/Components/Subtract.png"
+}
+;
 const Navbar = () => {
 
     return (
     <div className= {styles.navitem}>
         <div className = {styles.imgcont}>
-            <img src = '/Users/ibrahimarogundade/Desktop/gym_pages/Gympage/src/Subtract.png' alt = ''/>
+            <p>Gymbro</p>
+            {/* <img src ={images.image1} alt = ''/> */}
+            
         </div>
-        <ul>
-            <li>About Us</li>
-            <li>Let's Talk</li>
-        </ul>
+        <nav>
+            <ul>
+                <li>About Us</li>
+                <li>Let's Talk</li>
+            </ul>
+        </nav>
 
     </div>
     )
