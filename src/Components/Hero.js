f700bfb5a1bbf9d487874e47413bb57e3b5743a2
Ogunriwa <134988853+Ogunriwa@users.noreply.github.com>
2023-12-27 08:59:38 -0600

    trfr

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,21 +1,36 @@
 import React from "react";
-import styles from "./hero.module.css"
-
+import styles from "./hero.module.css";
 
 const Hero = () => {
   return (
     <div className={styles.heroitem}>
-        <div className={styles.strength}>
-            <p>Strength </p>
-            <p>When you make your fitness <br></br> journey a priority, we're here <br></br> to help you make it a reality</p>
+      <div className={styles.heron}>
+        <div className={styles.strenj}>
+          <h1>Strength</h1>
+          <p>
+            When you make your fitness<br /> journey a priority, we're here <br /> to
+            help you make it a reality
+          </p>
         </div>
-        <p>Consistency, Excellence</p>
+        <div className={styles.concel}>
+          <h1>Consistency, Excellence,</h1>
+        </div>
         <div className={styles.unleashed}>
-            <p>We can help you create a <br></br> personalized fitness plan <br></br> <br></br> <br></br> View Plans </p>
-            <p>Unleased</p>
+          <div className={styles.fitp}>
+            <div className={styles.perspl}>
+              <p>We can help you create a personalized fitness plan</p>
+            </div>
+            <div className={styles.plan}>
+              <h3>View Plans</h3>
+            </div>
+          </div>
+          <div className={styles.unleashedd}>
+            <h1>Unleashed</h1>
+          </div>
         </div>
+      </div>
     </div>
-  )  
-}
+  );
+};
 
 export default Hero;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,10 @@
 import React from "react";
 import styles from './Navbar.module.css';
+import frameImage from './Frame 37251.jpg';
 
 const images = {
-    image1 : "/Users/ibrahimarogundade/Desktop/gym_pages/Gympage/src/Components/Subtract.png"
+    image1 : "/Users/ibrahimarogundade/Desktop/gym_pages/Gympage/src/Components/Subtract.png",
+    image2 : "/Users/ibrahimarogundade/Desktop/gym_pages/Gympage/src/Components/Vector.png"
 }
 ;
 const Navbar = () => {
@@ -10,18 +12,27 @@ const Navbar = () => {
     return (
     <div className= {styles.navitem}>
         <div className = {styles.imgcont}>
-            <p>Gymbro</p>
-            {/* <img src ={images.image1} alt = ''/> */}
+            <p>Gymbro</p> 
+         
             
         </div>
         
-        <ul>
-            <li>About Us</li>
-            <li className={styles.letak}>Let's Talk</li>
-        </ul>
+        <div className={styles.menu}>
+            <a className= {styles.about}>About Us</a> 
+            <a className= {styles.letak}>
+            Let's Talk <img src = {frameImage}/></a>
+            
+            
+            
+        </div>
         
 
     </div>
     )
 };
 export default Navbar;
+
+
+
+// {/* <a className= {styles.letak}>Let's Talk</a>
+//             <img> src={images.image2}</img> */}
