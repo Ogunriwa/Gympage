f97daa65bbe88494adf04b72168b14b136e937cd
Ibrahim <ibrahimaro4@gmail.com>
2024-02-08 20:09:10 -0800

    clickreact

--- src/Components/Faq.js
+++ src/Components/Faq.js
@@ -1,9 +1,17 @@
 import React from "react";
 import styles from "./Faq.module.css";
-import chev from "./down.png"
+import chev from "./down.png";
+import { useState } from "react";
+
+
 
 const Faq = () => {
 
+    const [isActive, setIsActive] = useState(false);
+
+    const clickHandler = () => setIsActive(!isActive); 
+
+    
     return (
         <div className= {styles.last}>
 
@@ -11,12 +19,16 @@ const Faq = () => {
                 <h2>FAQ</h2>
                 <div className= {styles.questions}>
 
-                    <button>
-                        <div className= {styles.butdiv}>
+                    <div className= {styles.noclickdiv} onClick={clickHandler}>
+                        <div className= {styles.clickdiv}>
                             <h2>What are your qualifications</h2>
                             <img src ={chev}/>
                         </div>
-                    </button>
+                        {isActive && (<div className= {styles.ans}>
+                            <p>I am a certified trainer</p> 
+                        </div>)}
+                        
+                    </div>
                     <button>
                         <div className= {styles.butdiv}>
                             <h2>What are your qualifications</h2>
